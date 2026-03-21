751c777809e72b6ea3bb0d05d7f9686c8d40fc07
Santiago Fenochio <santifenochio@gmail.com>
2026-03-21 12:00:09 -0300

    chore: remove remaining temp files

--- _cleanup.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d C:\Users\Tino\Desktop\reflejos-de-la-ciudad
-"C:\Program Files\Git\bin\git.exe" add -A
-"C:\Program Files\Git\bin\git.exe" commit -m "chore: remove temp build scripts and logs"
-"C:\Program Files\Git\bin\git.exe" push origin master
-echo DONE:%ERRORLEVEL%
